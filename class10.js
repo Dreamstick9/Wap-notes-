c881bc9be186e7e18e9202b565e9c77de59c2aeb
Dreamstick <dreamstick909@gmail.com>
2026-02-13 22:26:16 +0530

    Add files via upload

---
+++ class10.js
@@ -0,0 +1,19 @@
+// console.log("wf")
+// setTimeout(function(){
+//     console.log("h");
+    
+// }, 6000)
+// setTimeout(function(){
+//     console.log("g");
+    
+// })
+
+console.log('A')
+setTimeout(()=>{
+    console.log('B')
+
+
+    setTimeout(()=>{
+        console.log('C')
+    })
+}, 1000)
