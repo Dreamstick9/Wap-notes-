2b3ab2d4e57c43e186d56739d5c7f421f2d13139
Dreamstick <dreamstick909@gmail.com>
2026-02-19 14:19:35 +0530

    Add files via upload

---
+++ class12.js
@@ -0,0 +1,19 @@
+// const user = {
+//     name: "Rahul",
+//     age: 25
+// };
+// let data1 = JSON.stringify(user)
+// console.log("object to json : ", data1)
+// const jsonString = '{"name": "Rahul", "age":25}';
+// let data2 = JSON.parse(jsonString)
+// console.log("json to object : ", data2);
+// const url = "https://dummyjson.com/products/193"
+// let v = fetch(url)
+
+// v.then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     console.log("Title : ", data.title)
+// }).catch((error)=>{
+//     console.log('Error: ',error)
+// })
